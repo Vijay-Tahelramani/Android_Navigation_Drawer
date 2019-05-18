ae00fd372cf7a0525da5d32a680484befd5010cb
vijay <vijaytahelramani999@gmail.com>
2019-05-18 16:37:36 -0400

    Navigation Drawer In Android

---
+++ NavigationDrawer/app/src/main/java/com/example/navigationdrawer/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.navigationdrawer;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        navigationView = (NavigationView)findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_camera:
+                Toast.makeText(this,"Camera Clicked...",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_gallery:
+                Toast.makeText(this,"Gallery Clicked...",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_slideshow:
+                Toast.makeText(this,"Slide Show Clicked...",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_manage:
+                Toast.makeText(this,"Tools Clicked...",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this,"Share Clicked...",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_send:
+                Toast.makeText(this,"Send Clicked...",Toast.LENGTH_LONG).show();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
